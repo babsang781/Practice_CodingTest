0430370fb8389465b7048122c9cc10548cda5807
박상민 <89438832+babsang781@users.noreply.github.com>
2022-05-01 23:19:29 +0900

    Update and rename 05.01-ex01-Q1.java to 05.01-ex01.java

--- Programmers_skill_ckeck/ch1/05.01-ex01.java
+++ Programmers_skill_ckeck/ch1/05.01-ex01.java
@@ -19,3 +19,23 @@ class Solution {
 //정확성: 50.0
 //효율성: 0.0 ??
 //합계: 50.0 / 50
+
+
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        for(int i = 1; i<n;i++){
+            if(n % i ==1){
+                answer = i;
+                break;
+            }
+                
+        }
+        return answer;
+    }
+}
+
+//채점 결과
+//정확성: 50.0
+//효율성: 0.0 ??
+//합계: 50.0 / 50
