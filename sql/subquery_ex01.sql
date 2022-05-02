dab3cce5ef1d055bfac0ac10a9130bd1c269e6d9
박상민 <89438832+babsang781@users.noreply.github.com>
2022-05-02 11:43:25 +0900

    Update and rename subquery_ex01.txt to subquery_ex01.sql

--- sql/subquery_ex01.sql
+++ sql/subquery_ex01.sql
@@ -1,3 +1,9 @@
+--보호소에서 중성화한 동물
 SELECT ANIMAL_ID, ANIMAL_TYPE, NAME 
 from (select * from ANIMAL_OUTS where SEX_UPON_OUTCOME like 'Spayed%' or SEX_UPON_OUTCOME like 'Neutered%')
 where ANIMAL_ID in (select ANIMAL_ID from ANIMAL_INS where SEX_UPON_INTAKE like ('Intact%'));
+
+--find Lucy and Ella
+SELECT ANIMAL_ID, NAME, SEX_UPON_INTAKE from ANIMAL_INS 
+where NAME in ('Lucy', 'Ella', 'Pickle', 'Rogan', 'Sabrina', 'Mitty') 
+order by ANIMAL_ID;
