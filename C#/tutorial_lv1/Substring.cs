1bce4a65d7d93ab9a66f0e643fc37f63b7cfbf0d
박상민 <89438832+babsang781@users.noreply.github.com>
2022-06-08 00:14:16 +0900

    Update Substring.cs

--- C#/tutorial_lv1/Substring.cs
+++ C#/tutorial_lv1/Substring.cs
@@ -1,7 +1,21 @@
-
-
-
-
+public class Solution {
+    public string solution(string s) {
+        // 가독성과 성능이 더 나은 코드를 고민하자.
+        // 변수 a 를 만들지 않고 매번 직접 s.Length 를 사용하면 코드는 1 줄 줄었지만 메모리와 시간적으로 성능이 안 좋아졌음. 
+        // 대체로 이 짧은 코드에서의 성능은 : 변수/2 연산까지 한 것 > 길이 변수만 > 변수 없이 매번 연산 
+        int a = s.Length / 2;
+        string answer="";
+        
+        if(s.Length % 2 == 0){
+            answer = s.Substring( a - 1, 2); // 2개 문자
+        }else{
+            answer = s.Substring( a, 1); // 1개 문자
+        }
+        
+        return answer;
+        
+    }
+}
 
 
 /*Java
